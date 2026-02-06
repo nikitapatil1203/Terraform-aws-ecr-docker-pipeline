bd2f6e4a2f27368047477d0da24135c989764f48
Nikita Patil <nikitapatil@Nikitas-MacBook-Air.local>
2026-02-06 19:36:43 +0530

    new changes

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
